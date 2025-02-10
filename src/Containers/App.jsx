6a79768b7ff5438e0477a47cfe8c73de1a4551c3
semajssor <141041402+semajssor@users.noreply.github.com>
2025-02-10 18:01:43 +0100

    Modified the import in App.jsx

--- src/Containers/App.jsx
+++ src/Containers/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import CardList from "../Components/CardList";
 import SearchBox from "../Components/SearchBox";
 import AnimatedGif from "../Components/AnimatedGif";
