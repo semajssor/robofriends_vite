0e254057945a319e501c42d09ed9e1d578b205b4
semajssor <141041402+semajssor@users.noreply.github.com>
2025-02-10 17:53:24 +0100

    Change the React code to use hooks

--- src/Containers/App.jsx
+++ src/Containers/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../Components/CardList";
 import SearchBox from "../Components/SearchBox";
 import AnimatedGif from "../Components/AnimatedGif";
@@ -6,52 +6,48 @@ import loadingGif from "../assets/loading.gif";
 import Scroll from "../Components/Scroll";
 import "./App.css";
 
-class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: [],
-			searchfield: "",
-		};
-	}
-	componentDidMount() {
-		fetch("https://jsonplaceholder.typicode.com/users")
-			.then((response) => {
-				if (!response.ok) {
-					throw new Error("Network response was not ok");
-				}
-				return response.json();
-			})
-			.then((users) => this.setState({ robots: users }))
-			.catch((error) => {
-				console.error("Error fetching data:", error);
-			});
-	}
+function App() {
+	const [robots, setRobots] = useState([]);
+	const [searchfield, setSearchfield] = useState("");
 
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value });
+
+	useEffect(() => {
+			fetch("https://jsonplaceholder.typicode.com/users")
+				.then((response) => {
+					if (!response.ok) {
+						throw new Error("Network response was not ok");
+					}
+					return response.json();
+				})
+				.then((users) => {setRobots(users)})
+				.catch((error) => {
+					console.error("Error fetching data:", error);
+				});
+	},[]);
+
+	const onSearchChange = (event) => {
+		setSearchfield(event.target.value);
 	};
-	render() {
-		const { robots, searchfield } = this.state;
-		const filteredRobots = robots.filter((robot) => {
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-		});
-		return !robots.length ? (
-			<div className="loading-container">
-				{" "}
-				<AnimatedGif src={loadingGif} alt="loading Pac Man Gif" />
-				{/* <AnimatedGif src="https://cdn.pixabay.com/animation/2022/10/11/03/16/03-16-39-160_512.gif" alt="loading" /> */}
-			</div>
-		) : (
-			<div className="tc">
-				<h1 className="f1">Robofriends</h1>
-				<SearchBox searchChange={this.onSearchChange} />
-				<Scroll>
-					<CardList robots={filteredRobots} />
-				</Scroll>
-			</div>
-		);
-	}
+	
+	const filteredRobots = robots.filter((robot) => {
+		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+	});
+	
+	return !robots.length ? (
+		<div className="loading-container">
+			{" "}
+			<AnimatedGif src={loadingGif} alt="loading Pac Man Gif" />
+			{/* <AnimatedGif src="https://cdn.pixabay.com/animation/2022/10/11/03/16/03-16-39-160_512.gif" alt="loading" /> */}
+		</div>
+	) : (
+		<div className="tc">
+			<h1 className="f1">Robofriends</h1>
+			<SearchBox searchChange={onSearchChange} />
+			<Scroll>
+				<CardList robots={filteredRobots} />
+			</Scroll>
+		</div>
+	);
 }
 
 export default App;
